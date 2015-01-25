0b88191a44dd788a66c7b919bdd5d71faf225269
pearlc <jinhochung@Jinhoui-MacBook-Pro.local>
2015-01-25 17:05:02 +0900

    activate 마크업 추가

---
+++ html/activate.php
@@ -0,0 +1,47 @@
+<?php
+include('header.php');
+?>
+
+<div id="main" class="container">
+
+    <div id="content" class="content bg-base section">
+
+        <div class="ribbon ribbon-highlight">
+            <ol class="breadcrumb ribbon-inner">
+                <li><a href="#">홈</a></li>
+                <li class="active" title="회원가입">회원가입</li>
+            </ol>
+        </div>
+
+        <header class="page-header">
+
+            <h1 class="page-title full-page-title">
+                회원가입
+            </h1>
+
+        </header>
+
+        <div class="row style-single">
+
+            <div class="col-sm-12 entry-content">
+
+                <div class="well no-padding col-md-4 col-md-offset-4">
+
+                    <h3>메일 인증 완료</h3>
+                    <p>이메일 인증이 완료되었습니다.</p>
+                    <p>로그인후 사이트를 계속 이용하세요.</p>
+                    <a href="#" class="btn btn-warning btn-block btn-lg">로그인</a>
+
+                </div>
+
+            </div><!--/.col-sm-12 entry-content-->
+
+        </div>
+
+    </div><!--/#content-->
+
+</div><!--#main.container-->
+
+<?php
+include('footer.php');
+?>
---
+++ html/activate_failed.php
@@ -0,0 +1,47 @@
+<?php
+include('header.php');
+?>
+
+<div id="main" class="container">
+
+    <div id="content" class="content bg-base section">
+
+        <div class="ribbon ribbon-highlight">
+            <ol class="breadcrumb ribbon-inner">
+                <li><a href="#">홈</a></li>
+                <li class="active" title="회원가입">회원가입</li>
+            </ol>
+        </div>
+
+        <header class="page-header">
+
+            <h1 class="page-title full-page-title">
+                회원가입
+            </h1>
+
+        </header>
+
+        <div class="row style-single">
+
+            <div class="col-sm-12 entry-content">
+
+                <div class="well no-padding col-md-4 col-md-offset-4">
+
+                    <h3>메일 인증 실패</h3>
+                    <p>이메일 인증 도중 문제가 발생했습니다.</p>
+                    <p>다시 시도해주세요.</p>
+                    <a href="#" class="btn btn-danger btn-block btn-lg">회원가입 다시하기</a>
+
+                </div>
+
+            </div><!--/.col-sm-12 entry-content-->
+
+        </div>
+
+    </div><!--/#content-->
+
+</div><!--#main.container-->
+
+<?php
+include('footer.php');
+?>
