86532f9824480ff21c615dd3cc985449132e7e21
pearlc <jinhochung@Jinhoui-MacBook-Pro.local>
2015-01-25 16:50:59 +0900

    인증메일 발송 페이지 생성

---
+++ html/register_done.php
@@ -0,0 +1,44 @@
+<?php
+include('header.php');
+?>
+
+<div id="main" class="container">
+
+    <div id="content" class="content bg-base section">
+
+        <div class="ribbon ribbon-highlight">
+            <ol class="breadcrumb ribbon-inner">
+                <li><a href="#">홈</a></li>
+                <li class="active" title="회원가입">회원가입</li>
+            </ol>
+        </div>
+
+        <header class="page-header">
+
+            <h1 class="page-title full-page-title">
+                회원가입
+            </h1>
+
+        </header>
+
+        <div class="row style-single">
+
+            <div class="col-sm-12 entry-content">
+
+                <div class="well no-padding col-md-6 col-md-offset-3">
+                    <h3>인증 메일 발송</h3>
+                    <p>입력하신 이메일 주소로 메일이 발송되었습니다.</p>
+                    <p>메일을 인증해서 회원가입을 마무리해주세요!</p>
+                </div>
+
+            </div><!--/.col-sm-12 entry-content-->
+
+        </div>
+
+    </div><!--/#content-->
+
+</div><!--#main.container-->
+
+<?php
+include('footer.php');
+?>
