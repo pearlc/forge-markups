2cf08f21bdd2caf37ae3ebbedfb64c1cb535a112
pearlc <jinhochung@Jinhoui-MacBook-Pro.local>
2015-02-06 17:19:49 +0900

    위키 메인 페이지

--- html/wiki.php
+++ html/wiki.php
@@ -1,7 +1,397 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jinhochung
- * Date: 15. 1. 31.
- * Time: 오후 3:05
- */
+include('header.php');
+?>
+
+<div id="main" class="container">
+    <div id="content" class="content section row">
+
+        <div class="col-md-9 bg-base col-lg-9 col-xl-10">
+
+            <div class="ribbon ribbon-highlight">
+                <ol class="breadcrumb ribbon-inner">
+                    <li><a href="#">홈</a></li>
+                    <li class="active">위키</li>
+                </ol>
+            </div>
+
+            <div class="entries">
+
+                <article class="entry style-media media type-post">
+
+                    <figure class="media-object pull-left entry-thumbnail hidden-xs">
+
+                        <!-- to disable lazy loading, remove data-src and data-src-retina -->
+                        <img src="http://placehold.it/230x230" data-src="http://placehold.it/230x230" data-src-retina="http://placehold.it/460x460" width="230" height="230" class="" alt="" style="opacity: 1;">
+
+                        <!--fallback for no javascript browsers-->
+                        <noscript>
+                            &lt;img src="http://placehold.it/230x230" alt=""&gt;
+                        </noscript>
+
+                    </figure>
+
+                    <div class="media-body">
+
+                        <header class="entry-header">
+                            <h3 class="entry-title">
+                                <a href="news_1.php" rel="bookmark">백투더맥 블로그가 페이스북에 전용 그룹을 마련했습니다</a>
+                            </h3>
+
+                            <div class="entry-meta">
+                                마지막 수정
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                                <!--                                <span class="author">by <a href="blog.html">John Doe</a></span>-->
+                                <!--                                <span class="entry-date"><a href="blog.html">on <time datetime="2013-07-04T23:26:34+00:00">September 20 2013</time></a></span>-->
+                                <!--                                <span class="category">In <a href="blog.html">Entertaintment</a></span>-->
+                            </div>
+                        </header>
+
+                        <p>
+                            안녕하세요, 백투더맥 블로그 운영자 ONE™ 입니다. 얼마전 출시한 OS X 요세미티와 함께 즐거운 맥라이프 하고들 계신가요? 블로그를 처음 개설할 때 만해도 OS X 10.7 라이언이 최신 버전이었는데, 이후 마운틴 라이언과 매버릭스, 그리고 이번에 나온 요..
+                            <a href="news_1.php" class="more-link">더보기<i class="icon icon-long-arrow-right"></i></a>
+                        </p>
+
+                    </div>
+
+                </article>
+
+                <article class="entry style-media media type-post">
+
+                    <figure class="media-object pull-left entry-thumbnail hidden-xs">
+
+                        <!-- to disable lazy loading, remove data-src and data-src-retina -->
+                        <img src="http://placehold.it/230x230" data-src="http://placehold.it/230x230" data-src-retina="http://placehold.it/460x460" width="230" height="230" class="" alt="" style="opacity: 1;">
+
+                        <!--fallback for no javascript browsers-->
+                        <noscript>
+                            &lt;img src="http://placehold.it/230x230" alt=""&gt;
+                        </noscript>
+
+                    </figure>
+
+                    <div class="media-body">
+
+                        <header class="entry-header">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">차세대 12인치 맥북에어 부품사진 추가 유출... '메탈 재질의 애플로고'</a>
+                            </h3>
+
+                            <div class="entry-meta">
+                                마지막 수정
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                                <!--                                <span class="author">by <a href="blog.html">John Doe</a></span>-->
+                                <!--                                <span class="entry-date"><a href="blog.html">on <time datetime="2013-07-04T23:26:34+00:00">September 20 2013</time></a></span>-->
+                                <!--                                <span class="category">In <a href="blog.html">Entertaintment</a></span>-->
+                            </div>
+                        </header>
+
+                        <p>
+                            어제 12인치 맥북에어 디스플레이 파트를 담은 사진이 뭉텅이로 유출돼 국내외 맥 커뮤니티를 깜짝 놀라게 했는데요, 오늘은 애플로고 부품으로 보이는 사진이 추가로 올라왔습니다. 프랑스 IT매체 'Nowheree​lse' 사이트에 애플로고 부품 사진으로 추정되는..
+                            <a href="single.html" class="more-link">더보기<i class="icon icon-long-arrow-right"></i></a>
+                        </p>
+
+                    </div>
+
+                </article>
+
+                <article class="entry style-media media type-post">
+
+                    <figure class="media-object pull-left entry-thumbnail hidden-xs">
+
+                        <!-- to disable lazy loading, remove data-src and data-src-retina -->
+                        <img src="http://placehold.it/230x230" data-src="http://placehold.it/230x230" data-src-retina="http://placehold.it/460x460" width="230" height="230" class="" alt="" style="opacity: 1;">
+
+                        <!--fallback for no javascript browsers-->
+                        <noscript>
+                            &lt;img src="http://placehold.it/230x230" alt=""&gt;
+                        </noscript>
+
+                    </figure>
+
+                    <div class="media-body">
+
+                        <header class="entry-header">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">12인치 신형 맥북에어 부품사진 유출!... 12인치 레티나 디스플레이 채택</a>
+                            </h3>
+
+                            <div class="entry-meta">
+                                마지막 수정
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                                <!--                                <span class="author">by <a href="blog.html">John Doe</a></span>-->
+                                <!--                                <span class="entry-date"><a href="blog.html">on <time datetime="2013-07-04T23:26:34+00:00">September 20 2013</time></a></span>-->
+                                <!--                                <span class="category">In <a href="blog.html">Entertaintment</a></span>-->
+                            </div>
+                        </header>
+
+                        <p>
+                            국내 보안업체인 '안랩'이 2014년 맥 플랫폼의 보안 이슈와 동향을 분석한 보고서를 공식 블로그에 개제했습니다. 팔은 안으로 굽는다고 맥용 V3 제품군에 대한 홍보 성격이 가미되어 있지만, 지난 한해 발생한 크고 작은 보안 위협 형태와 악성코드 작동 원리..
+                            <a href="single.html" class="more-link">더보기<i class="icon icon-long-arrow-right"></i></a>
+                        </p>
+
+                    </div>
+
+                </article>
+
+                <article class="entry style-media media type-post">
+
+                    <figure class="media-object pull-left entry-thumbnail hidden-xs">
+
+                        <!-- to disable lazy loading, remove data-src and data-src-retina -->
+                        <img src="http://placehold.it/230x230" data-src="http://placehold.it/230x230" data-src-retina="http://placehold.it/460x460" width="230" height="230" class="" alt="" style="opacity: 1;">
+
+                        <!--fallback for no javascript browsers-->
+                        <noscript>
+                            &lt;img src="http://placehold.it/230x230" alt=""&gt;
+                        </noscript>
+
+                    </figure>
+
+                    <div class="media-body">
+
+                        <header class="entry-header">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">차세대 12인치 맥북에어 부품사진 추가 유출... '메탈 재질의 애플로고'</a>
+                            </h3>
+
+                            <div class="entry-meta">
+                                마지막 수정
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                                <!--                                <span class="author">by <a href="blog.html">John Doe</a></span>-->
+                                <!--                                <span class="entry-date"><a href="blog.html">on <time datetime="2013-07-04T23:26:34+00:00">September 20 2013</time></a></span>-->
+                                <!--                                <span class="category">In <a href="blog.html">Entertaintment</a></span>-->
+                            </div>
+                        </header>
+
+                        <p>
+                            안녕하세요, 백투더맥 블로그 운영자 ONE™ 입니다. 얼마전 출시한 OS X 요세미티와 함께 즐거운 맥라이프 하고들 계신가요? 블로그를 처음 개설할 때 만해도 OS X 10.7 라이언이 최신 버전이었는데, 이후 마운틴 라이언과 매버릭스, 그리고 이번에 나온 요..
+                            <a href="single.html" class="more-link">더보기<i class="icon icon-long-arrow-right"></i></a>
+                        </p>
+
+                    </div>
+
+                </article>
+
+                <article class="entry style-media media type-post">
+
+                    <figure class="media-object pull-left entry-thumbnail hidden-xs">
+
+                        <!-- to disable lazy loading, remove data-src and data-src-retina -->
+                        <img src="http://placehold.it/230x230" data-src="http://placehold.it/230x230" data-src-retina="http://placehold.it/460x460" width="230" height="230" class="" alt="" style="opacity: 1;">
+
+                        <!--fallback for no javascript browsers-->
+                        <noscript>
+                            &lt;img src="http://placehold.it/230x230" alt=""&gt;
+                        </noscript>
+
+                    </figure>
+
+                    <div class="media-body">
+
+                        <header class="entry-header">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">백투더맥 블로그가 페이스북에 전용 그룹을 마련했습니다</a>
+                            </h3>
+
+                            <div class="entry-meta">
+                                마지막 수정
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                                <!--                                <span class="author">by <a href="blog.html">John Doe</a></span>-->
+                                <!--                                <span class="entry-date"><a href="blog.html">on <time datetime="2013-07-04T23:26:34+00:00">September 20 2013</time></a></span>-->
+                                <!--                                <span class="category">In <a href="blog.html">Entertaintment</a></span>-->
+                            </div>
+                        </header>
+
+                        <p>
+                            어제 12인치 맥북에어 디스플레이 파트를 담은 사진이 뭉텅이로 유출돼 국내외 맥 커뮤니티를 깜짝 놀라게 했는데요, 오늘은 애플로고 부품으로 보이는 사진이 추가로 올라왔습니다. 프랑스 IT매체 'Nowheree​lse' 사이트에 애플로고 부품 사진으로 추정되는..
+                            <a href="single.html" class="more-link">더보기<i class="icon icon-long-arrow-right"></i></a>
+                        </p>
+
+                    </div>
+
+                </article>
+
+            </div><!--/.entris-->
+
+<!--            <ul class="pagination">-->
+<!--                <li><a href="#">«</a></li>-->
+<!--                <li><a href="#">1</a></li>-->
+<!--                <li><a href="#">2</a></li>-->
+<!--                <li class="active"><span>3</span></li>-->
+<!--                <li><a href="#">4</a></li>-->
+<!--                <li><a href="#">5</a></li>-->
+<!--                <li><a href="#">6</a></li>-->
+<!--                <li><a href="#">7</a></li>-->
+<!--                <li><a href="#">8</a></li>-->
+<!--                <li><a href="#">9</a></li>-->
+<!--                <li><a href="#">10</a></li>-->
+<!--                <li><a href="#">5</a></li>-->
+<!--                <li><span> ... </span></li>-->
+<!--                <li><a href="#">1234</a></li>-->
+<!--                <li><a href="#">1235</a></li>-->
+<!--                <li><a href="#">»</a></li>-->
+<!--            </ul>-->
+
+        </div><!--/.col-md-8.col-lg-8.col-xl-9-->
+
+        <div class="sidebar col-md-3 col-lg-3 col-xl-2">
+
+            <aside class="widget">
+
+                <h2 class="widget-title ribbon"><span>최근 편집된 위키 문서</span></h2>
+
+                <div class="entries row">
+
+                    <article class="type-post style-media-list media col-sm-6 col-md-12">
+
+                        <div class="media-body">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">장비 추천 가이드 - 허리띠</a>
+                            </h3>
+                            <div class="entry-meta pull-right">
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                            </div>
+                        </div>
+
+                    </article>
+
+                    <article class="type-post style-media-list media col-sm-6 col-md-12">
+
+                        <div class="media-body">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">무림맹 작전 지령서 탈취</a>
+                            </h3>
+                            <div class="entry-meta pull-right">
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                            </div>
+                        </div>
+
+                    </article>
+
+                    <article class="type-post style-media-list media col-sm-6 col-md-12">
+
+                        <div class="media-body">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">바람의 전장(혼천교)</a>
+                            </h3>
+                            <div class="entry-meta pull-right">
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                            </div>
+                        </div>
+
+                    </article>
+
+                    <article class="type-post style-media-list media col-sm-6 col-md-12">
+
+                        <div class="media-body">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">혼천교 분타를 함락하라!</a>
+                            </h3>
+                            <div class="entry-meta pull-right">
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                            </div>
+                        </div>
+
+                    </article>
+
+                    <article class="type-post style-media-list media col-sm-6 col-md-12">
+
+                        <div class="media-body">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">등장인물</a>
+                            </h3>
+                            <div class="entry-meta pull-right">
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                            </div>
+                        </div>
+
+                    </article>
+
+                </div>
+
+            </aside><!--/.widget-->
+
+            <aside class="widget">
+
+                <h2 class="widget-title ribbon"><span>최근 게시판 글</span></h2>
+
+                <div class="entries row">
+
+                    <article class="type-post style-media-list media col-sm-6 col-md-12">
+
+                        <div class="media-body">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">북미섭) 배치 어제 끝냈습니다 (실4 -> 브2)</a>
+                            </h3>
+                            <div class="entry-meta pull-right">
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                            </div>
+                        </div>
+
+                    </article>
+
+                    <article class="type-post style-media-list media col-sm-6 col-md-12">
+
+                        <div class="media-body">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">배치후 랭겜 60판째..</a>
+                            </h3>
+                            <div class="entry-meta pull-right">
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                            </div>
+                        </div>
+
+                    </article>
+
+                    <article class="type-post style-media-list media col-sm-6 col-md-12">
+
+                        <div class="media-body">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">어제 실수로 룬과 특성을 반대로 가져갔는데요</a>
+                            </h3>
+                            <div class="entry-meta pull-right">
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                            </div>
+                        </div>
+
+                    </article>
+
+                    <article class="type-post style-media-list media col-sm-6 col-md-12">
+
+                        <div class="media-body">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">페이커의 고향은 플레 1이가 봅니다.</a>
+                            </h3>
+                            <div class="entry-meta pull-right">
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                            </div>
+                        </div>
+
+                    </article>
+
+                    <article class="type-post style-media-list media col-sm-6 col-md-12">
+
+                        <div class="media-body">
+                            <h3 class="entry-title">
+                                <a href="single.html" rel="bookmark">새 시즌을 맞아서 세일하고 있네요~ </a>
+                            </h3>
+                            <div class="entry-meta pull-right">
+                                <time datetime="2013-07-04T23:26:34+00:00">2014-01-23 09:20:12</time>
+                            </div>
+                        </div>
+
+                    </article>
+
+                </div>
+
+            </aside><!--/.widget-->
+
+        </div><!--/.sidebar col-md-4 col-lg-4 col-xl-3-->
+
+    </div><!--/#content-->
+
+</div><!--#main.container-->
+
+<?php
+include('footer.php');
+?>
---
+++ html/wiki_create.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jinhochung
+ * Date: 15. 2. 6.
+ * Time: 오후 5:19
+ */
